e2d165207e1d2a0bb47d3c48b35af2c048526236
sharianakram <sharianakram0503@gmail.com>
2024-12-06 21:44:45 -0500

    update

--- nested-queries/nested.sql
+++ nested-queries/nested.sql
@@ -13,3 +13,27 @@ FROM northwind.Orders
 WHERE ShipVIA = (SELECT ShipperID FROM northwind.Shippers WHERE CompanyName = 'Federal Shipping');
 
 -- question 3
+
+SELECT *
+FROM northwind.`order details`
+WHERE ProductID = (SELECT ProductID 
+					FROM northwind.products
+                    WHERE ProductName = "Sasquatch Ale");
+			
+SELECT * 
+FROM northwind.orders;
+-- question 4
+
+SELECT ContactName
+FROM northwind.customers
+WHERE CustomerID = (SELECT CustomerID
+					FROM northwind.orders
+                    WHERE OrderID = 10266);
+                    
+-- question 5
+SELECT FirstName,LastName,Title,Salary
+FROM northwind.employees
+WHERE EmployeeID = (SELECT EmployeeID
+					FROM northwind.orders
+                    WHERE OrderID = 10266);
+
